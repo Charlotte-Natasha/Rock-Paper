d02922ea7a757618eb80a573279fdf4958f56743
Charlotte Natasha <natashaonyango97@gmail.com>
2022-12-10 23:50:22 +0100

    added playRound selection

--- script.js
+++ script.js
@@ -12,4 +12,55 @@ function playerPlay(e){
 
     result = playRound(playerChoice, computerPlay())
     console.log(result);
+
+    document.getElementById("current_play").innerHTML = result;
+    document.getElementById("player_score").innerHTML = playerScore;
+    document.getElementById("computer_score").innerHTML = computerScore;
 }
+
+const computerPlay = () => {
+    const randomNumber = Math.floor(Math.random()*3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 0:
+            return "rock";
+        case 0:
+            return "rock";        
+    }
+};
+
+const playRound = (playerSelection, computerSelection) => {
+    
+    if (playerSelection === computerSelection) {
+        return `It's a tie! <br> You played: ${playerSelection.toUpperCase()}. Computer played: ${computerSelection.toUpperCase()}`;
+    }
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            computerScore += 1;
+            return `Sorry player, Computer won! <br> You played: ${playerSelection.toUpperCase()}. Computer played: ${computerSelection.toUpperCase()} `;
+        } else {
+            playerScore += 1;
+            return `Congratulations, you won! <br> You played: ${playerSelection.toUpperCase()}. Computer played: ${computerSelection.toUpperCase()}`;
+        }
+    }
+
+    if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            computerScore += 1;
+            return `Sorry player, Computer won! <br> You played: ${playerSelection.toUpperCase()}. Computer played: ${computerSelection.toUpperCase()}`;
+        } else {
+            playerScore +=1
+            return `Congratulations, you won! <br> You played: ${playerSelection.toUpperCase()}. Computer played: ${computerSelection.toUpperCase()}`;
+        }
+    }
+    if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            computerScore += 1;
+            return `Sorry player, Computer won! <br> You played: ${playerSelection.toUpperCase()}. Computer played: ${computerSelection.toUpperCase()}`;
+        } else {
+            playerScore += 1;
+            return `Congratulations, you won! <br> You played: ${playerSelection.toUpperCase()}. Computer played: ${computerSelection.toUpperCase()}`;
+        }
+    }
+};
