a1cb07985e48b553291b069edffe7bf1aee69b51
Charlotte Natasha <natashaonyango97@gmail.com>
2022-12-10 23:10:57 +0100

    added playerPlay function

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let buttons = document.querySelectorAll('.btn')
 buttons.forEach(function(currentBtn){
     currentBtn.addEventListener('click', playerPlay)
 })
+
+function playerPlay(e){
+    let playerChoice = e.target.id;
+    console.log(`Player choice is : ${playerChoice}`);
+
+    result = playRound(playerChoice, computerPlay())
+    console.log(result);
+}
